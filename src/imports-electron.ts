3bc5311a5c1f688acb18d94beed90073c4c3dd30
gilad <giladbrn@gmail.com>
2019-11-18 13:48:21 +0200

    before trying to no dist

--- src/imports-electron.ts
+++ src/imports-electron.ts
@@ -1,5 +1,6 @@
-import { PythonShell } from "python-shell";
+// import { PythonShell } from "python-shell";
+import * as PS from "python-shell";
 
-
-console.log('PS: ', PythonShell);
+console.log(PS);
+// console.log('PS: ', pythonShell);
 // remote.globalShortcut.register('CommandOrControl+Y', openDevTools);
