13c48aa8cd20f9391ca698b86e34f03167551651
gilad <giladbrn@gmail.com>
2019-11-24 20:21:08 +0200

    Input extends Div

--- declarations/pages/New/sections/subject.d.ts
+++ declarations/pages/New/sections/subject.d.ts
@@ -1,5 +1,12 @@
-import { Div } from "../../../bhe";
+import { Div, Span } from "../../../bhe";
+declare class Input extends Div {
+    editable: Span;
+    constructor();
+    private autoComplete;
+    private onKeyDown;
+}
 declare class SubjectDiv extends Div {
+    input: Input;
     constructor({ id }: {
         id: any;
     });
--- src/pages/New/sections/subject.ts
+++ src/pages/New/sections/subject.ts
@@ -5,18 +5,43 @@
  * sections.subject*/
 import { div, elem, button, span, Div, Button, Span } from "../../../bhe";
 
+class Input extends Div {
+    editable: Span;
+    
+    constructor() {
+        super({ cls : 'input' });
+        const editable = span({ cls : 'editable' })
+            .attr({ contenteditable : true })
+            .on({ input : (ev: InputEvent) => this.autoComplete(ev) });
+        this
+            .attr({ contenteditable : true })
+            .click(() => this.editable.focus())
+            .on({ keydown : (e: KeyboardEvent) => this.onKeyDown(e) })
+            .cacheAppend({
+                editable,
+                autocomplete : span({ cls : 'autocomplete', text : 'Subject Id' })
+            });
+    }
+    
+    private autoComplete(ev: InputEvent) {
+        console.log('autoComplete', ev);
+    }
+    
+    private onKeyDown(e: KeyboardEvent) {
+        console.log('onKeyDown', e);
+    }
+}
+
+
 class SubjectDiv extends Div {
+    input: Input;
     
     
     constructor({ id }) {
         super({ id });
-        this.cacheAppend({
-            input : div({ cls : 'input' }).cacheAppend({
-                editable : span({ cls : 'editable' }).attr({ contenteditable : true }),
-                autocomplete : span({ cls : 'autocomplete', text : 'Subject Id' })
-            }),
-            
-        })
+        
+        const input = new Input();
+        this.cacheAppend({ input })
     }
     
     
