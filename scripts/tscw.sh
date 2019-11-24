b7a8350c32fab772265294f3d3b02b161dd6ac1b
gilad <giladbrn@gmail.com>
2019-11-24 17:01:36 +0200

    before making a package.json inside src

--- scripts/tscw.sh
+++ scripts/tscw.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-node_modules/typescript/bin/tsc -p . --watch
+node_modules/typescript/bin/tsc -p tsconfig.json --watch
--- src/pages/New/index.ts
+++ src/pages/New/index.ts
@@ -1,7 +1,12 @@
 import Glob from "../../Glob";
+import * as util from '../../util'
 
 async function load(reload: boolean) {
     const { exam, test } = Glob.BigConfig;
+    Glob.BigConfig.last_page = "new";
+    if ( reload ) {
+        util.reloadPage();
+    }
     // exam.finished_trials_count = 0;
     // test.finished_trials_count = 0;
     // current_test.finished_trials_count = 0;
