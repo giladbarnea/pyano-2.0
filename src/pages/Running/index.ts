20e1fb64847b8d98488c67e346953d813b8048d6
gilad <giladbrn@gmail.com>
2019-11-26 19:06:55 +0200

    palys midi

--- src/pages/Running/index.ts
+++ src/pages/Running/index.ts
@@ -3,10 +3,11 @@ import * as util from "../../util";
 import { elem } from "../../bhe";
 import animation from './animation'
 import Dialog from './dialog'
-// import { Piano } from "../../Piano"
+import { Piano } from "../../Piano"
 import { Midi } from "@tonejs/midi";
+import * as Tone from "tone";
 
-const { Piano } = require("@tonejs/piano");
+// const { Piano } = require("@tonejs/piano");
 
 /**import * as runningPage from "../Running"
  * require('./Running')*/
@@ -22,15 +23,32 @@ async function load(reload: boolean) {
     const subconfig = Glob.BigConfig.getSubconfig();
     const piano = new Piano({
         samples : SALAMANDER_PATH_ABS,
-        release : false,
-        pedal : false,
-        velocities : 1,
+        release : true,
+        pedal : true,
+        velocities : 5,
     }).toDestination();
     await piano.load();
     console.log('piano loaded');
     const midi = await Midi.fromUrl(subconfig.truth.midi.absPath);
     console.log('midi loaded');
-    console.log({ subconfig, midi, piano });
+    const noteOffEvents = new Tone.Part((time, event) => {
+        piano.keyUp(event.note, time);
+    }, midi.tracks[0].notes.map(n => ({
+        note : n.name,
+        time : n.time + n.duration,
+    }))).start(0);
+    
+    const noteOnEvents = new Tone.Part((time, event) => {
+        piano.keyDown(event.note, time, event.velocity);
+        // piano.keyUp(event.note, time + event.duration)
+    }, midi.tracks[0].notes.map(n => ({
+        note : n.name,
+        velocity : n.velocity,
+        duration : n.duration,
+        time : n.time,
+    }))).start(0);
+    Tone.Transport.start();
+    console.log({ subconfig, midi, piano, "Tone.Transport.state" : Tone.Transport.state, });
     Glob.Title.html(`${subconfig.truth.name}`);
     
     const subtitle = elem({ tag : 'h3', text : '1/1' });
