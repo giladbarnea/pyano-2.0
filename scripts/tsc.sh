9b2fd28c9e1b5891fbbdb804b057a289a3efbcbb
Gilad Barnea <giladbrn@gmail.com>
2020-12-03 19:10:48 +0200

    scripts/tsc.sh: instead of 'find .. -exec ..', now 'find .. | while ..'

--- scripts/tsc.sh
+++ scripts/tsc.sh
@@ -31,7 +31,7 @@ while [[ $# -gt 0 ]]; do
     ;;
   *)
     echo "$_help"
-    echo "\nFATAL: unknown arg: $1. exiting."
+    echo "FATAL: unknown arg: $1. exit 1"
     exit 1
     ;;
   esac
@@ -40,26 +40,43 @@ set -- "${POSITIONAL[@]}"
 
 printf "\nkilling possible tsc processes...\n---------------------------------\n"
 for proc in $(pgrep -f "tsc "); do
-  kill $proc && printf "\nkilled %s\n" $proc || printf "\nWARN: failed killing %s\n" $proc
+  if kill $proc; then
+    printf "\nkilled %s\n" $proc
+  else
+    printf "\nWARN: failed killing %s\n" $proc
+  fi
 done
+
 source ./scripts/rm_tscompiled_files.sh
 
 printf "\nrunning tsc...\n--------------\n"
 ./node_modules/typescript/bin/tsc -p . &
 wait $!
-printf "\ncopying non-js files to dist...\n---------------------------------\n"
+printf "\ncopying non-ts files to dist...\n---------------------------------\n"
 if [[ ! -e ./dist ]]; then
-  mkdir dist && printf "\nWARN: dist dir did not exist, created it\n" || ( printf "\nFATAL: 'dist' dir did not exist and failed creating it\n" && exit 1 )
+  if mkdir dist; then
+    printf "\nWARN: dist dir did not exist, created it\n"
+  else
+    printf "\nFATAL: 'dist' dir did not exist and failed creating it\n"
+    exit 1
+  fi
 fi
-cp -r ./src/* ./dist && printf "\ncopied all files in src into dist\n" || ( printf "\nFATAL: failed copying all files in src into dist\n" && exit 1 )
-if find . -type f -regextype posix-extended -regex "\./dist/.*[^.]*\.ts$" -exec rm "{}" ";"; then
-  echo "removed all .ts files from 'dist' dir"
+if cp -r ./src/* ./dist; then
+  printf "\ncopied all files from src into dist, now removing .ts files from dist...\n"
 else
-  echo "FATAL: failed removing all .ts files from 'dist' dir"
+  printf "\nFATAL: failed copying all files in src into dist\n"
   exit 1
 fi
-printf "\nfinished copying\n"
-function check_iwatch_or_die(){
+find . -type f -regextype posix-extended -regex "\./dist/.*[^.]*\.ts$" | while read -r tsfile; do
+  if rm "$tsfile"; then
+    echo "\tremoved '$tsfile' successfully"
+  else
+    echo "\tWARN: failed removing '$tsfile'"
+  fi
+done
+
+printf "\nfinished copying non-ts files to dist\n"
+function check_iwatch_or_die() {
   if ! command -v 'iwatch'; then
     printf "\nFATAL: 'iwatch' not found. exiting."
     exit 1
@@ -78,11 +95,17 @@ if [[ "$tscwatch" == true ]]; then
     check_iwatch_or_die
     iwatch -c 'python3 ./scripts/fix_d_ts_reference_types.py %f' -t '.*\.d\.ts$' -r declarations
   fi
-else
+else # no watch
   if [[ "$remove_use_strict" == true ]]; then
-    find . -type f -regextype posix-extended -regex "\./dist/.*\.js$" -exec python3 ./scripts/remove_use_strict.py "{}" ";"
+    find . -type f -regextype posix-extended -regex "\./dist/.*\.js$" | while read -r jsfile; do
+      python3 ./scripts/remove_use_strict.py "$jsfile"
+    done
+    #    find . -type f -regextype posix-extended -regex "\./dist/.*\.js$" -exec python3 ./scripts/remove_use_strict.py "{}" ";"
   fi
   if [[ "$fix_d_ts_reference_types" == true ]]; then
-    find . -type f -regextype posix-extended -regex "\./declarations/.*\.d\.ts$" -exec python3 ./scripts/fix_d_ts_reference_types.py "{}" ";"
+    find . -type f -regextype posix-extended -regex "\./declarations/.*\.d\.ts$" | while read -r dtsfile; do
+      python3 ./scripts/fix_d_ts_reference_types.py "$dtsfile"
+    done
+    #    find . -type f -regextype posix-extended -regex "\./declarations/.*\.d\.ts$" -exec python3 ./scripts/fix_d_ts_reference_types.py "{}" ";"
   fi
 fi
